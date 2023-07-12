ea37f26b7db4caa99dec19363dcb613f75b489c4
mrwebwork <tech@allanswebwork.info>
2023-07-12 15:17:16 -0500

    Create send message buttons and functions

--- allans-messenger/components/MessageBar.jsx
+++ allans-messenger/components/MessageBar.jsx
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+
+import toast from "react-hot-toast";
 
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
 
 export default function MessageBar() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSendMessage = (e) => {
+    if (!inputValue) return;
+    e.preventDefault();
+    setInputValue("");
+    toast.success("Message Sent!");
+  };
+
   return (
-    <div className="my-10 h-4/6 w-5/6">
+    <form onSubmit={handleSendMessage} className="my-10 h-4/6 w-5/6">
       <div className="flex flex-col items-center">
         <div className="w-full max-w-lg flex flex-row-reverse items-center justify-between">
           <BsFillArrowRightCircleFill
-            onClick={() => {}}
+            onClick={handleSendMessage}
             className="cursor-pointer"
             size={32}
           />
           <input
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
             className="w-full p-3 me-3 rounded-xl outline-none border-2 border-sky-200 focus:border-sky-400"
             type="text"
             placeholder="Start typing here. . ."
           />
         </div>
       </div>
-    </div>
+    </form>
   );
 }
