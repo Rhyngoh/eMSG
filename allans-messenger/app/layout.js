1edcd88b9bceb0cc6b985714fbab7d74d30a7a0b
mrwebwork <tech@allanswebwork.info>
2023-07-12 05:13:37 -0500

    Add favicon.ico image

--- allans-messenger/app/layout.js
+++ allans-messenger/app/layout.js
@@ -1,7 +1,11 @@
-import { Toaster } from "react-hot-toast";
 import "./globals.css";
 
+import Head from "next/head";
+
+import { Toaster } from "react-hot-toast";
+
 import { Montserrat } from "next/font/google";
+
 import { AuthContextProvider } from "@/context/AuthContext";
 
 const mont = Montserrat({ subsets: ["latin"] });
@@ -15,6 +19,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{metadata.title}</title>
+        <description>{metadata.description}</description>
+        <link rel="icon" href="allans-messenger/app/favicon.ico" />
+        <meta name="description" content={metadata.description} />
+      </Head>
       <body className={mont.className}>
         <AuthContextProvider>{children}</AuthContextProvider>
         <Toaster />
