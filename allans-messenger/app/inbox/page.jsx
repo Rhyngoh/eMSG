5b9f5a337f7b03320a40a58043c6d4d8e937b9e3
mrwebwork <tech@allanswebwork.info>
2023-07-12 07:58:42 -0500

    Build out Inboxes message page

--- allans-messenger/app/inbox/page.jsx
+++ allans-messenger/app/inbox/page.jsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react";
 
 import { useAuthContext } from "@/context/AuthContext";
 
+import MessageBar from "@/components/MessageBar";
+
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 
@@ -32,7 +34,8 @@ export default function Page() {
 
   return (
     <>
-      <div className="flex flex-row-reverse justify-between px-10 py-10">
+      {/* Header */}
+      <div className="flex flex-row-reverse justify-around lg:justify-between px-2 lg:px-10 py-10">
         {/* User Information */}
         <div className="">
           {user && (
@@ -42,11 +45,46 @@ export default function Page() {
             </>
           )}
         </div>
+        <div className="hidden sm:flex  sm:text-2xl lg:text-5xl text-center font-bold  lg:hidden">
+          eMSG
+        </div>
+        <div className="text-2xl lg:text-5xl text-center font-bold hidden lg:flex">
+          eMessages Chat Room
+        </div>
         {/* Button */}
         <div>
           <Link className="btn-4" href="/signin">
             Sign Out
           </Link>
+        </div>
+      </div>
+      <hr />
+
+      {/* Title */}
+      <div>
+        <p className=" pt-7 text-4xl font-bold text-center"> Messages </p>
+      </div>
+
+      {/* Chat Wrapper */}
+      <div className="flex flex-col justify-around items-center h-screen lg:flex-row-reverse lg:items-start">
+        {/* Messages Wrapper */}
+        <div className="bg-neutral-100 ms-4 me-10 my-10 h-4/6 w-5/6 rounded-lg shadow-xl">
+          <div className="flex flex-col items-center h-screen">
+            {/* Chat Area */}
+            <div className="bg-white rounded-md mt-7 h-5/6 w-5/6 lg:w-4/6">
+              <div className="h-72 w-full"> </div>
+            </div>
+
+            {/* Message Bar */}
+            <MessageBar />
+          </div>
+        </div>
+        {/* Conversations */}
+        <div className="my-10 mx-5">
+          <div className="border-x-red-300 lg:border-x-8 lg:px-7 lg:h-screen rounded-xl">
+            <p className="text-4xl font-bold">Conversations</p>
+            <hr className="mt-5" />
+          </div>
         </div>
       </div>
     </>
---
+++ allans-messenger/components/MessageBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
+
+export default function MessageBar() {
+  return (
+    <div className="my-10 h-4/6 w-5/6">
+      <div className="flex flex-col items-center">
+        <div className="w-full max-w-lg flex flex-row-reverse items-center justify-between">
+          <BsFillArrowRightCircleFill
+            onClick={() => {}}
+            className="cursor-pointer"
+            size={32}
+          />
+          <input
+            className="w-full p-3 me-3 rounded-xl outline-none border-2 border-sky-200"
+            type="text"
+            placeholder="Start typing here. . ."
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
